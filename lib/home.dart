0e2c4ec696ca2e3843b55fe949e45b051021ec01
bowie <leticia.bini@gmail.com>
2023-08-19 19:34:56 -0300

    commit

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'login.dart';
-import 'campoTexto.dart';
+import 'package:video_player/video_player.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -19,11 +18,105 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
     curve: Curves.easeInSine,
   );
 
+  late final VideoPlayerController _videoController;
+
+  @override
+  void initState() {
+    super.initState();
+    _videoController = VideoPlayerController.asset("assets/foguete.mp4")
+      ..initialize().then((_) {
+        // Ensure the first frame is shown before playing
+        setState(() {});
+      });
+  }
+
   @override
   void dispose() {
+    _videoController.dispose();
     _controller.dispose();
     super.dispose();
   }
+
+  void _showAlert() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        final screenHeight = MediaQuery.of(context).size.height;
+        final dialogHeight = screenHeight * 0.5;
+
+        return AlertDialog(
+          contentPadding: EdgeInsets.zero,
+          content: Container(
+            height: dialogHeight,
+            width: double.maxFinite,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Flexible(
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      AspectRatio(
+                        aspectRatio: 16 / 9,
+                        child: Stack(
+                          children: [
+                            VideoPlayer(_videoController),
+                            Align(
+                              alignment: Alignment.center,
+                              child: IconButton(
+                                onPressed: () {
+                                  if (_videoController.value.isPlaying) {
+                                    _videoController.pause();
+                                  } else {
+                                    _videoController.play();
+                                  }
+                                },
+                                icon: Icon(
+                                  _videoController.value.isPlaying
+                                      ? Icons.pause
+                                      : Icons.play_arrow,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Positioned(
+                        top: 20,
+                        child: Container(
+                          padding: EdgeInsets.all(10),
+                          color: Colors.black.withOpacity(0.7),
+                          child: Text(
+                            "Este é um Easter Egg:\nOlhe como é por dentro do foguete",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 16),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  style: ElevatedButton.styleFrom(primary: Colors.amberAccent),
+                  child: Text("Voltar"),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -33,26 +126,31 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
         child: SizedBox(
           child: Column(
             children: [
-              Center(
-                child: Container(
-                  width: 350,
-                  padding: EdgeInsets.all(5),
-                  margin: EdgeInsets.all(5),
-                  decoration: BoxDecoration(
-                    border: Border.all(width: 3, color: Colors.white12),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: SizeTransition(
-                    sizeFactor: _animation,
-                    axis: Axis.vertical,
-                    axisAlignment: -1,
-                    child: Image.asset(
-                      "assets/foguete.jpeg",
-                      color: Colors.yellowAccent[100],
-                      colorBlendMode: BlendMode.darken,
-                      fit: BoxFit.cover,
-                      width: 400,
-                      height: 600,
+              GestureDetector(
+                onTap: () {
+                  _showAlert();
+                },
+                child: Center(
+                  child: Container(
+                    width: 350,
+                    padding: EdgeInsets.all(5),
+                    margin: EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 3, color: Colors.white12),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: SizeTransition(
+                      sizeFactor: _animation,
+                      axis: Axis.vertical,
+                      axisAlignment: -1,
+                      child: Image.asset(
+                        "assets/foguete.jpeg",
+                        color: Colors.yellowAccent[100],
+                        colorBlendMode: BlendMode.darken,
+                        fit: BoxFit.cover,
+                        width: 400,
+                        height: 600,
+                      ),
                     ),
                   ),
                 ),
@@ -84,11 +182,11 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
         ),
       ),
       bottomNavigationBar: BottomAppBar(
-        color: Colors.black45,
+        color: Colors.white12,
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 16.0),
           child: Row(
-            mainAxisAlignment: MainAxisAlignment.center, // Centralizar os botões
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               ElevatedButton(
                 onPressed: () {
@@ -100,7 +198,7 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
                   style: TextStyle(color: Colors.black45),
                 ),
               ),
-              SizedBox(width: 16), // Adicionar espaço entre os botões
+              SizedBox(width: 16),
               ElevatedButton(
                 onPressed: () {
                   Navigator.pushNamed(context, '/cadastro');
