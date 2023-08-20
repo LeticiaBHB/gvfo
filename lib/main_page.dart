4081c30a52193469c653d11b361470b0db44b72f
bowie <leticia.bini@gmail.com>
2023-08-20 15:58:29 -0300

    commit

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+
 class MainPage extends StatefulWidget {
-  const MainPage({super.key});
+  MainPage({Key? key}) : super(key: key);
 
   @override
   State<MainPage> createState() => _MainPageState();
@@ -9,23 +10,41 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-      return SafeArea(
-        child: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.black45,
-            title: Text('Meu perfil'),),
-            drawer: const Drawer(
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                  Text('dados cadastrais')
-                  ],
-                ),
-              ),
-            ),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black45,
+          title: Text('Meu perfil'),
         ),
-      );
+        drawer: Drawer(
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                  ),
+                  child: ClipOval(
+                    child: Image.asset(
+                      "assets/foguetinho.jpeg", width: 100,
+                      height: 100,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 30),
+                Text('Dados Cadastrais'),
+                SizedBox(height: 30),
+                Text('Configurações'),
+                SizedBox(height: 30),
+                Text('Termos de uso e Privacidade'),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
