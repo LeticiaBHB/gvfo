c4ae171a7de814dc93fd4edf97c92a5a1b9feea7
bowie <leticia.bini@gmail.com>
2023-08-21 17:57:09 -0300

    commit

--- lib/shared/widgets/custon_drawer.dart
+++ lib/shared/widgets/custon_drawer.dart
@@ -15,26 +15,26 @@ class CustonDrawer extends StatelessWidget {
           InkWell(
             onTap: () {
               showModalBottomSheet(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10)
-                ),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
                   context: context,
                   builder: (BuildContext bc) {
                     return Wrap(
                       children: [
-                       ListTile(
-                         onTap:(){
-                           Navigator.pop(context);
-                         },
-                         title: Text('Câmera'),
-                         leading: Icon(Icons.camera_alt_outlined),
-                       ),
                         ListTile(
-                          onTap: (){
+                          onTap: () {
                             Navigator.pop(context);
                           },
-                            title: Text('Galeria'),
-                        leading: Icon(Icons.photo_album),)
+                          title: Text('Câmera'),
+                          leading: Icon(Icons.camera_alt_outlined),
+                        ),
+                        ListTile(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          title: Text('Galeria'),
+                          leading: Icon(Icons.photo_album),
+                        )
                       ],
                     );
                   });
@@ -112,7 +112,77 @@ class CustonDrawer extends StatelessWidget {
                 ],
               ),
             ),
-            onTap: () {},
+            onTap: () {
+              showModalBottomSheet(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  context: context,
+                  builder: (BuildContext bc) {
+                    return Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 12, vertical: 16),
+                      child: ListView(
+                        children: [
+                          Center(
+                            child: Text(
+                              'Termos de uso e Privacidade',
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.w500),
+                            ),
+                          ),
+                          SizedBox(height: 20),
+                          RichText(
+                            text: TextSpan(
+                              children: [
+                                TextSpan(
+                                  text:
+                                      '  ', // Espaços em branco para criar o recuo
+                                ),
+                                TextSpan(
+                                  text:
+                                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
+                                      'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  Proin mollis efficitur enim a eleifend. Sed molestie lectus quis sagittis facilisis. Quisque porttitor dictum orci, vitae rutrum mauris tempus in. Nam mattis ipsum dolor, ut condimentum massa ultrices nec. Sed laoreet, lacus eu vulputate auctor, odio risus malesuada magna, quis ultricies ante arcu id erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a lacus maximus, luctus metus sit amet, ornare ex. Null.',
+                                  style: TextStyle(color: Colors.black),
+                                ),
+                              ],
+                            ),
+                          ),
+                          RichText(
+                            text: TextSpan(
+                              children: [
+                                TextSpan(
+                                  text:
+                                      '  ', // Espaços em branco para criar o recuo
+                                ),
+                                TextSpan(
+                                  text:
+                                      'Cras consequat diam ut augue scelerisque, at cursus magna varius. Vivamus nec accumsan erat.'
+                                      'nec lobortis ipsum. Vestibulum quam risus, feugiat id molestie non, cursus in tortor. Praesent quis vulputate orci. Nam ac nulla eu lectus consectetur volutpat eleifend in tellus. Quisque suscipit nisi condimentum velit pulvinar, a bibendum lorem posuere. Sed malesuada hendrerit est. Quisque dignissim dolor id nulla vehicula, vitae efficitur sem eleifend. Proin fermentum velit purus, at sodales felis rhoncus et. Etiam mattis elit erat, mattis condimentum nibh mollis ut. Maecenas sapien diam, rhoncus eu placerat vel, pretium porta lacus. Pellentesque fringilla faucibus tellus eget vestibulum. Morbi enim purus, tempus eu dui eget, ullamcorper pulvinar tortor. ',
+                                  style: TextStyle(color: Colors.black),
+                                ),
+                              ],
+                            ),
+                          ),
+                          RichText(
+                            text: TextSpan(
+                              children: [
+                                TextSpan(
+                                  text:
+                                      '  ', // Espaços em branco para criar o recuo
+                                ),
+                                TextSpan(
+                                  text:
+                                      'Praesent accumsan enim id dui euismod lobortis. Proin mollis efficitur enim a eleifend. Sed molestie lectus quis sagittis facilisis. Quisque porttitor dictum orci, vitae rutrum mauris tempus in. Nam mattis ipsum dolor, ut condimentum massa ultrices nec. Sed laoreet, lacus eu vulputate auctor, odio risus malesuada magna, quis ultricies ante arcu id erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a lacus maximus, luctus metus sit amet, ornare ex. Nulla facilisi. Donec finibus, velit a fringilla imperdiet, ante orci facilisis eros, a pharetra augue metus non metus. ',
+                                  style: TextStyle(color: Colors.black),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  });
+            },
           ),
           SizedBox(height: screenHeight * 0.2),
           Center(
