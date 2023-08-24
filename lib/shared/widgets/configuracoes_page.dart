4ba0326191c4c18020002c04c9c675b783b2b416
bowie <leticia.bini@gmail.com>
2023-08-24 20:36:20 -0300

    commit

--- lib/shared/widgets/configuracoes_page.dart
+++ lib/shared/widgets/configuracoes_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 class ConfiguracoesPage extends StatefulWidget {
   const ConfiguracoesPage({super.key});
 
@@ -7,12 +8,30 @@ class ConfiguracoesPage extends StatefulWidget {
 }
 
 class _ConfiguracoesPageState extends State<ConfiguracoesPage> {
+  late SharedPreferences storage;
+
   TextEditingController nomeUsuarioController = TextEditingController();
   TextEditingController alturaController = TextEditingController();
   String? nomeUsuario;
   double? altura;
   bool receberNotificacoes = false;
   bool temaEscuro = false;
+
+  final CHAVE_NOME_USUARIO = 'CHAVE_NOME_USUARIO';
+  final CHAVE_ALTURA = 'CHAVE_ALTURA';
+  final CHAVE_RECEBER_NOTIFICACOES = 'CHAVE_RECEBER_NOTIFICACOES';
+  final CHAVE_MODO_ESCURO = 'CHAVE_MODO_ESCURO';
+
+  @override
+  void initState (){
+    super.initState();
+    carregarDados();
+  }
+
+  carregarDados() async {
+    storage = await SharedPreferences.getInstance();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -24,14 +43,14 @@ class _ConfiguracoesPageState extends State<ConfiguracoesPage> {
             child: ListView(
               children: [
                 Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
                   child: TextField(
                     decoration: InputDecoration(hintText: 'Nome Usuário'),
                     controller: nomeUsuarioController,
                   ),
                 ),
                 Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
                   child: TextField(
                     keyboardType: TextInputType.number,
                     decoration: InputDecoration(hintText: 'Identificação'),
@@ -53,7 +72,13 @@ class _ConfiguracoesPageState extends State<ConfiguracoesPage> {
                     onChanged: (bool value){
                       setState(() {temaEscuro = value;});
                     }),
-
+                TextButton(onPressed: () async {
+                  await storage.setString(CHAVE_NOME_USUARIO, nomeUsuarioController.text);
+                  await storage.setDouble(CHAVE_ALTURA, double.tryParse(alturaController.text) ?? 0);
+                  await storage.setBool(CHAVE_RECEBER_NOTIFICACOES, receberNotificacoes);
+                  await storage.setBool(CHAVE_MODO_ESCURO, temaEscuro);
+                }, child: Text('Salvar')
+                  ,)
               ],
             ),
           ),
