0dd396991f10eb3d2b1a82d2dad3204514708daa
bowie <leticia.bini@gmail.com>
2023-08-28 16:40:36 -0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 import 'home.dart';
 import 'campoTexto.dart';
 import 'login.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  var documentsDiretory = await path_provider.getApplicationCacheDirectory();
+  Hive.init(documentsDiretory.path);
   runApp(MyApp());
 }
 
